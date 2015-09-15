2bdf6981367d9b0723ccc4ee801faddf9cc655fa
SKKeller <Sophia.Keller@uni-duesseldorf.de>
2015-09-15 11:34:17 +0200

    erstellt vollständige tupleversion

--- src/table_to_b/core.clj
+++ src/table_to_b/core.clj
@@ -9,52 +9,126 @@
   [& args]
   (println "Hello, World!"))
 
+(def workbook
+  "läd spreadsheet.xlsx"
+  (load-workbook "spreadsheet.xlsx"))
 
-(defn test1 [x]
-  (->> (load-workbook "spreadsheet.xlsx")
-       (select-sheet "Price List")
-       (select-columns {:B :element})))
+(def s
+  "tabelle Price List"
+  (select-sheet "Price List" workbook))
 
-(defn number-of-columns [first-row]
-  (count (cell-seq first-row)))
+(def datatype
+  "Zuordnung Datentypnr -> Bezeichnung"
+  {:0 "INTEGER" :1 "STRING" :2 "Formula" :3 "Blank" :4 "BOOLEAN" :5 "Error"})
 
-(defn nth-column-name [n]
-  (nth (map (comp keyword str) (map char (range 65 (+ 65 26)))) n))
+(def tupleanfang
+  "String für den Anfang der Tupleversion"
+  (str "MACHINE ExcelTable_TupleVersion" \newline "DEFINITIONS TUPLETYPE == ("))
 
+(defn get-datatype
+  "ordnet der Typnummer den Datentyp zu"
+  [typenr]
+  (get datatype ((comp keyword str) typenr)))
 
-(defn nth-column
-  "Bekommt ein Spreadsheet, gibt die n. Reihe als Vektor von Hashmaps mit Keyword :element wieder."
-  [n sheet]
-  (let [name (nth-column-name n)]
-    (select-columns {name name} sheet)))
+(defn element
+  "Gibt das übergebene Element mit Anführungszeichen zurück, wenn es vom Typ String ist
+  sonst ohne"
+  [el]
+  (if (= (type el) java.lang.String)
+    (str \" el \")
+    el))
 
-                                        ; bekommt eine Reihe als List und die Anzahl der Elemente
-                                        ; gibt ein tuple der Elemente wieder
-(defn row-as-tuple [row]
-  (loop [elements row erg ""]
-    (if (empty? elements)
-      (str "(" (subs erg 2) ")")
-      (recur (rest elements) (str erg ", " (let [el (first elements)] (if (= (type el) java.lang.String) (str \" el \") el )))))))
+(defn row-as-tuple
+  "Gibt die Daten einer Reihe als Tuple zurück
+  angepasst nach Typ mit Anführungszeichen oder ohne"
+  [row]
+  (str "(" (reduce str (interpose ", " (map element row))) ")" \newline))
 
-                                        ; bekommt Reihen
-                                        ; gibt einen String wieder, der die Reihen als Tuple wiedergibt, getrennt durch Zeilenumbrüche
-(defn rows-as-tuples [rows]
-  (loop [el rows tuple ""]
-    (if (empty? el)
-      tuple
-      (recur (rest el) (str tuple \newline (row-as-tuple (map read-cell (first el))))))))
+(defn rows-as-tuples
+  "bekommt Reihen, gibt einen String wieder, der die Reihen als tuple wiedergibt
+  getrennt durch Zeilenumbrueche"
+  [rows]
+  (reduce str (map row-as-tuple rows)))
 
-(defn get-cell [n row]
-  (.getCell row n))
-
-(defn get-type [cell]
+(defn get-type
+  "Gibt den Typ der übergeben Zelle als Zahl zurück, Zuordnung siehe 'datatype'"
+  [cell]
   (when cell
     (.getCellType cell)))
 
-(defn types-of-nth-column [n sheet]
+(defn get-cell
+  "Gibt den Inhalt der n. Zelle Zeile row wieder"
+  [n row]
+  (.getCell row n))
+
+(defn types-of-nth-column
+  "Gibt die Typen der Zellen der n. Spalte der Tabelle sheet als Zahlen wieder"
+  [n sheet]
   (map (comp get-type (partial get-cell n)) (row-seq sheet)))
 
-(types-of-nth-column 0 s)
+(defn haupttyp
+  "Gibt den Namen der typen wieder, wenn er bei allen gleich ist,
+  sonst 'STRING'"
+  [typen]
+  (if (apply = typen)
+    (get-datatype (first typen))
+    "STRING"))
+
+(defn anzahl-Reihen
+  "Gibt Anzahl der Reihen der Tabellenseite wieder"
+  [sheet]
+  (count (row-seq sheet)))
+
+(defn haupttypen
+  "Gibt die Datentypen der Reihen der Tabelle wieder"
+  [sheet]
+  (loop [i (anzahl-Reihen sheet) erg ()]
+    (if (> 0 i)
+      erg
+      (recur (dec i) (conj erg (haupttyp (types-of-nth-column i sheet))) ))))
+
+(defn dateibegin
+  "Erstellt den Anfang der B-Datei"
+  [sheet]
+  (str tupleanfang (reduce str (interpose "*" (haupttypen sheet))) ")" ))
+
+(defn generate-properties
+  "Erstellt den Properties Teil für Tuples"
+  [rows]
+  (str "PROPERTIES" \newline
+  "Excel : POW(TUPLETYPE) & /* Typing */" \newline
+  "Excel = { /* the Data */" \newline
+  (rows-as-tuples (rest rows)) " }"))
+
+(defn generate-function
+  "erstellt das Grundgerüst der Projektionsfunction für Tuples"
+  [n]
+  (let [vars (reduce str (interpose ",x" (range 0 (inc n))))]
+    (str " = %(x" vars ").((x" vars "):TUPLETYPE|x")))
+
+(defn tuples-functions
+  "erstellt die projection functions für die Tupleversion"
+  [row]
+  (let [function (generate-function (count (map read-cell row)))]
+    (loop [i 0 cells (map read-cell row) erg ""]
+      (if (empty? cells)
+        erg
+        (recur (inc i) (rest cells) (str erg (first cells) function i ")" \newline))))))
+
+(defn abstract-constants
+  "erstellt den ABSTRACT_CONSTANTS-Teil"
+  [row]
+  (str "ABSTRACT_CONSTANTS " (reduce str (interpose ", " (map read-cell row))) \newline "CONSTANTS Excel" \newline ))
+
+(defn erstelle-tuplemachine
+  "erstellt eine b-machine aus der angegebenen Tabelle und speichert sie unter test.txt"
+  [sheet]
+  (let [rows (row-seq sheet)]
+    (spit "test.txt"
+      (str (dateibegin sheet)  \newline
+      (abstract-constants (first rows)) \newline
+      (generate-properties rows) \newline
+      (tuples-functions (first rows))))))
 
 (defn first-row [dateiname sheetname]
   (let [workbook (load-workbook (str dateiname))
@@ -62,7 +136,4 @@
         columns (select-columns {:A :name, :B :price, :C :wert} sheet)
         rows (row-seq sheet)]
     (sheet-name (first (sheet-seq workbook)))
-    (number-of-columns (first rows))
-    (map :element (nth-column 1 sheet))
-                                        ;(spit "test.txt" (rows-as-tuples rows))
     ))
